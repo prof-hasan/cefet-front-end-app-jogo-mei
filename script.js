17ed9172702f630919d988311f0e2d6e59a079b0
Felwalter <felwalter70@gmail.com>
2025-11-26 22:17:43 -0300

    Melhorias no visual e jogabilidade
    Visualização do saldo e sistema de cartas

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ let personagens = [
         agua: 4,
         defesa: 2,
         dinheiro: 4,
-        local: "Periferia"
+        local: "Periferia",
+        idade: "",
+        condicao: ""
     },
     {
         nome: "Gabriel",
@@ -23,7 +25,9 @@ let personagens = [
         agua: 8,
         defesa: 9,
         dinheiro: 7,
-        local: "AreaPriv"
+        local: "AreaPriv",
+        idade: "",
+        condicao: "Asma"
     },
     {
         nome: "Luanda",
@@ -31,7 +35,9 @@ let personagens = [
         agua: 4,
         defesa: 3,
         dinheiro: 2,
-        local: "Quilombo"
+        local: "Quilombo",
+        idade: "",
+        condicao: ""
     },
     {
         nome: "Kaike",
@@ -39,7 +45,9 @@ let personagens = [
         agua: 6,
         defesa: 5,
         dinheiro: 2,
-        local: "Aldeia"
+        local: "Aldeia",
+        idade: "",
+        condicao: ""
     },
     {
         nome: "Maria",
@@ -47,7 +55,9 @@ let personagens = [
         agua: 2,
         defesa: 3,
         dinheiro: 2,
-        local: "Periferia"
+        local: "Periferia",
+        idade: "",
+        condicao: ""
     },
     {
         nome: "Adriana",
@@ -55,7 +65,9 @@ let personagens = [
         agua: 4,
         defesa: 4,
         dinheiro: 4,
-        local: ""
+        local: "",
+        idade: "",
+        condicao: ""
     },
     {
         nome: "Tereza",
@@ -63,14 +75,29 @@ let personagens = [
         agua: 7,
         defesa: 5,
         dinheiro: 3,
-        local: ""
+        local: "",
+        idade: "",
+        condicao: ""
+    },
+    {
+        nome: "Roberto",
+        vida: 2,
+        agua: 10,
+        defesa: 8,
+        dinheiro: 10,
+        local: "",
+        idade: "Avançada",
+        condicao: ""
     },
     {
         nome: "Hasan",
         vida: 10,
         agua: 10,
         defesa: 10,
-        dinheiro: 10
+        dinheiro: 10,
+        local: "",
+        idade: "",
+        condicao: ""
     },
 ];
 
@@ -78,32 +105,44 @@ let personagens = [
 let cartasEvt = [
     {
         nome: "calorE",
-        id: 0
+        id: 0,
+
     },
     {
         nome: "poluicaoDA",
-        id: 1
+        id: 1,
+
     },
     {
         nome: "queimadaEAN",
-        id: 2
+        id: 2,
+
     },
     {
         nome: "surtoDH",
-        id: 3
+        id: 3,
+
     },
     {
         nome: "tempestadeCA",
-        id: 4
+        id: 4,
+
     }
 ];
 
 //Banco de cartas de sorte ou azar
-let cartasSorte {
+let cartasSorte = [
     {
-        
+        num: 0,
+        vida: 0,
+        agua: 0,
+        defesa: 0,
+        id: 1
     },
-};
+    {
+        id: 2
+    }
+];
 
 let vez = 0;
 let turno = 0;
@@ -195,38 +234,44 @@ function escolhePersonagem() {
                 return personagemAtual;
             }
         }
-        else if(rNumber >= 1 / 7 && rNumber < 1 / 7 * 2) {
+        else if(rNumber >= 1 / 8 && rNumber < 1 / 8 * 2) {
             personagemAtual = personagens[1];
             if (!checaJogadores(personagemAtual)) {
                 return personagemAtual;
             }
         }
-        else if(rNumber >= 1 / 7 * 2 && rNumber < 1 / 7 * 3) {
+        else if(rNumber >= 1 / 8 * 2 && rNumber < 1 / 8 * 3) {
             personagemAtual = personagens[2];
             if (!checaJogadores(personagemAtual)) {
                 return personagemAtual;
             }
         }
-        else if(rNumber >= 1 / 7 * 3 && rNumber < 1 / 7 * 4) {
+        else if(rNumber >= 1 / 8 * 3 && rNumber < 1 / 8 * 4) {
             personagemAtual = personagens[3];
             if (!checaJogadores(personagemAtual)) {
                 return personagemAtual;
             }
         }
-        else if(rNumber >= 1 / 7 * 4 && rNumber < 1 / 7 * 5) {
+        else if(rNumber >= 1 / 8 * 4 && rNumber < 1 / 8 * 5) {
             personagemAtual = personagens[4];
             if (!checaJogadores(personagemAtual)) {
                 return personagemAtual;
             }
         }
-        else if(rNumber >= 1 / 7 * 5 && rNumber < 1 / 7 * 6) {
+        else if(rNumber >= 1 / 8 * 5 && rNumber < 1 / 8 * 6) {
             personagemAtual = personagens[5];
             if (!checaJogadores(personagemAtual)) {
                 return personagemAtual;
             }
         }
-        else if(rNumber >= 1 / 7 * 6 && rNumber < 1) {
+        else if(rNumber >= 1 / 8 * 6 && rNumber < 1 / 8 * 7) {
             personagemAtual = personagens[6];
+            if (!checaJogadores(personagemAtual)) {
+                return personagemAtual;
+            }
+        }
+        else if(rNumber >= 1 / 8 * 7 && rNumber < 1) {
+            personagemAtual = personagens[7];
             if (!checaJogadores(personagemAtual)) {
                 return personagemAtual;
             }
@@ -269,7 +314,8 @@ function criaJogador(nJogador) {
         agua: personagemA.agua,
         defesa: personagemA.defesa,
         dinheiro: personagemA.dinheiro,
-        local: personagemA.local
+        local: personagemA.local,
+        saldo: {sVida: 0, sAgua: 0, sDefesa: 0, sDinheiro: 0}
     }
 
     return jogador;
@@ -296,6 +342,25 @@ function atualizaHTMLJogador(jogador) {
         jogadorBarraWater.innerHTML = rotuloInnerAtributo(1, jogador.agua);
         jogadorBarraDefense.innerHTML = rotuloInnerAtributo(2, jogador.defesa);
         jogadorBarraMoney.innerHTML = rotuloInnerAtributo(3, jogador.dinheiro);
+
+        let atualizaSaldo = function(saldo, i) {
+            let saldoText = document.createElement("em");
+            saldoText.classList.add("atributo-saldo");
+
+            saldoText.innerHTML = saldo == 0 ? "" : saldo > 0 ? '+' + saldo : saldo;
+
+            if (Number(saldoText.innerText) > 0)
+                saldoText.style.color = "green";
+            else
+                saldoText.style.color = "red";
+
+            labelAtNode[i].appendChild(saldoText);
+        };
+
+        atualizaSaldo(jogador.saldo.sVida, 0);
+        atualizaSaldo(jogador.saldo.sAgua, 1);
+        atualizaSaldo(jogador.saldo.sDefesa, 2);
+        atualizaSaldo(jogador.saldo.sDinheiro, 3);
     }
     else {
         jogadorBarraLife.innerHTML = `Saúde: ${jogador.vida}/10`;
@@ -308,22 +373,101 @@ function atualizaHTMLJogador(jogador) {
     jogadorName.innerHTML = jogador.nome;
 }
 
-let cartaEvtDisplay = 0;
+let cartaDisplay = 0;
 
-function ativaCartaEvt(carta) {
+
+//Ativa o efeito da carta de envento no jogador
+function 
+efeitoCartaEvt(carta, jogador) {
+    if (carta.id == 0) {
+        if (jogador.vida < 7) {
+            jogador.saldo.sAgua--;
+        }
+        if (jogador.vida < 4) {
+            jogador.saldo.sDinheiro--;
+        }
+        if (jogador.dinheiro > 7) {
+            jogador.saldo.dinheiro -= 2;
+        }
+    }
+
+    else if (carta.id == 1) {
+        if (jogador.idade == "Avançada" || jogador.condicao == "Asma") {
+            jogador.saldo.sVida--;
+            jogador.saldo.sDinheiro -= 2;
+        }
+        if (jogador.vida < 5) {
+            jogador.saldo.sDinheiro--;
+        }
+    }
+
+    else if (carta.id == 2) {
+        if (jogador.idade == "Avançada" || jogador.condicao == "Asma") {
+            jogador.saldo.sVida--;
+            jogador.saldo.sDinheiro -= 2;
+        }
+        if (jogador.agua < 5) {
+            jogador.saldo.sVida--;
+        }
+        if (jogador.defesa < 6) {
+            jogador.saldo.sAgua--;
+        }
+    }
+
+    else if (carta.id == 3 && jogador.vida <= 7) {
+        if (jogador.agua < 4 && jogador.defesa < 4) {
+            jogador.saldo.sVida -= 2;
+        }
+        if (jogador.dinheiro > 6) {
+            jogador.saldo.sDinheiro -= 2;
+        }
+    }
+
+    else if (carta.id == 4) {
+        if (jogador.defesa < 6) {
+            jogador.saldo.sDefesa--;
+        }
+        if (jogador.agua < 5) {
+            jogador.saldo.sAgua--;
+        }
+    }
+
+    atualizaHTMLJogador(jogadores[vez]);
+}
+
+//Mostra a carta na tela e chama a função de efeito
+function ativaCarta(carta, tipo) {
+    cartaDisplay = 1;
     let containerCarta = document.createElement("div");
-    containerCarta.classList.add("cartaevt-container");
+
+    if (tipo == 1)
+        containerCarta.classList.add("cartaevt-container");
+    else
+        containerCarta.classList.add("cartasorte-container");
+
     bodyEl.appendChild(containerCarta);
 
-    let cartaEvtFrente = document.createElement("img");
-    cartaEvtFrente.classList.add("cartaevt-frente");
-    cartaEvtFrente.src = `imgs/cartasEvts/SQPlogo.png`;
-    containerCarta.appendChild(cartaEvtFrente);
+    let cartaFrente = document.createElement("img");
+    cartaFrente.classList.add("carta-frente");
 
-    let cartaEvtTras = document.createElement("img");
-    cartaEvtTras.classList.add("cartaevt-tras");
-    cartaEvtTras.src = `imgs/cartasEvts/${carta.nome}.png`;
-    containerCarta.appendChild(cartaEvtTras);
+    cartaFrente.src = `imgs/cartasEvts/SQPlogo.png`;
+
+    let cartaTras = document.createElement("img");
+    cartaTras.classList.add("carta-tras");
+
+    if (tipo == 1) {
+        cartaTras.src = `imgs/cartasEvts/${carta.nome}.png`;
+        cartaTras.style.borderRadius = "20px";
+        cartaFrente.style.borderRadius = "20px";
+    }
+    else {
+        cartaTras.src = `imgs/cartasSA/SorteouAzar-${carta.id}.png`;
+        cartaTras.style.borderRadius = "40px";
+        cartaFrente.style.borderRadius = "40px";
+    }
+
+    containerCarta.appendChild(cartaFrente);
+    containerCarta.appendChild(cartaTras);
 
     bodyEl.style.overflowY = "hidden";
 
@@ -333,24 +477,26 @@ function ativaCartaEvt(carta) {
         containerCarta.style.transition = "all 0.5s ease-in-out";
     }, 300);
 
-    cartaEvtDisplay = 1;
-
     setTimeout(() => {
         containerCarta.style.transform = "translate(-50%, -50%) rotateY(180deg)";
     }, 1000);
 
     let funcaoRemove = function() {
-        if (cartaEvtDisplay == 1) {
+        if (cartaDisplay == 1) {
             
             containerCarta.style.transform = "translate(-50%, 0) rotateY(180deg)";
             containerCarta.style.top = "150%";
 
+            clearTimeout(autoTimeOut);
+
             setTimeout(() => {
                 bodyEl.removeChild(containerCarta);
                 bodyEl.removeEventListener("dblclick", funcaoRemove);
-                cartaEvtDisplay = 0;
                 bodyEl.style.overflowY = "auto";
-            }, 800);
+                cartaDisplay = 0;
+            }, 500);
+
+            
         }
     };
 
@@ -358,52 +504,66 @@ function ativaCartaEvt(carta) {
         bodyEl.addEventListener("dblclick", funcaoRemove);
     }, 1500);
 
-    setTimeout(funcaoRemove, 20000);
+    let autoTimeOut = setTimeout(funcaoRemove, 20000);
 }
+
+
 
 //Passagem turnos
-
-let cartaEvtAtual = sorteiaCartaEvt();
-ativaCartaEvt(cartaEvtAtual);
-
-let turnosAjuste = -5;
-
-function passaVez() {
-    turnosAjuste++;
-    if (!(turnosAjuste < 0)) {
-        vez++;
-        console.log(vez);
-        if (vez === 5) {
-            vez = 0;
-            turnosAjuste = -6;
-            cartaEvtAtual = sorteiaCartaEvt();
-            ativaCartaEvt(cartaEvtAtual);
-        }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        jogadores[i].num--;
-        if (jogadores[i].num === 0)
-            jogadores[i].num = 5;
-    
-        atualizaHTMLJogador(jogadores[i]);
-    }
-
-    atNode = document.querySelectorAll(".fixo");
-
-    for (let i = 0; i < atNode.length; i++) {
-        atNode[i].addEventListener("mousedown", geraAt);
-        atNode[i].style.cursor = "grab";
-    }
-}
-
-botaoPassaEl.addEventListener("click", passaVez);
 
 for (let i = 0; i < 5; i++) {
     jogadores.push(criaJogador(i + 1));
     atualizaHTMLJogador(jogadores[vez + i]);
 }
 
+let cartaEvtAtual = sorteiaCartaEvt();
+ativaCarta(cartaEvtAtual, 1);
+
+let turnosAjuste = -10;
+
+function passaVez() {
+    if (!cartaDisplay) {
+        turnosAjuste++;
+
+        vez++;
+        if (vez === 5) {
+            vez = 0;
+            if (turnosAjuste == 0) {
+                turnosAjuste = -10;
+                cartaEvtAtual = sorteiaCartaEvt();
+                ativaCarta(cartaEvtAtual, 1);
+                for (let i = 0; i < jogadores.length; i++) {
+                    efeitoCartaEvt(cartaEvtAtual, jogadores[i]);
+                }
+            }
+        }
+
+        if (turnosAjuste >= -5) {
+            ativaCarta(cartasSorte[0], 0);
+        }
+        
+
+        for (let i = 0; i < 5; i++) {
+            jogadores[i].num--;
+            if (jogadores[i].num === 0)
+                jogadores[i].num = 5;
+        
+            atualizaHTMLJogador(jogadores[i]);
+        }
+
+        atNode = document.querySelectorAll(".fixo");
+
+        for (let i = 0; i < atNode.length; i++) {
+            atNode[i].addEventListener("mousedown", geraAt);
+            atNode[i].style.cursor = "grab";
+        }
+
+        console.log("ajuste ->", turnosAjuste);
+        console.log("vez ->", vez);
+    }
+}
+
+botaoPassaEl.addEventListener("click", passaVez);
 
 console.log(jogadores);
 
@@ -450,14 +610,22 @@ function geraAt(e) {
         bodyEl.appendChild(novaImg);
 
         if (atualEl.classList.contains("atributo-status")) {
-            if(atualEl.dataset.tipo == 0)
+            if(atualEl.dataset.tipo == 0) {
                 jogadores[vez].vida--;
-            else if(atualEl.dataset.tipo == 1)
+                jogadores[vez].saldo.sVida++;
+            }
+            else if(atualEl.dataset.tipo == 1) {
                 jogadores[vez].agua--;
-            else if(atualEl.dataset.tipo == 2)
+                jogadores[vez].saldo.sAgua++;
+            }
+            else if(atualEl.dataset.tipo == 2) {
                 jogadores[vez].defesa--;
-            else if(atualEl.dataset.tipo == 3)
+                jogadores[vez].saldo.sDefesa++;
+            }
+            else if(atualEl.dataset.tipo == 3) {
                 jogadores[vez].dinheiro--;
+                jogadores[vez].saldo.sDinheiro++;
+            }
 
             atualizaHTMLJogador(jogadores[vez]);
 
@@ -521,14 +689,22 @@ function paraAt() {
     let tipoImgAtual = img.dataset.tipo;
 
     if (Number(tipoImgAtual) == barraEmHover) {
-        if(tipoImgAtual == 0 && jogadores[vez].vida < 10)
+        if(tipoImgAtual == 0 && jogadores[vez].vida < 10) {
             jogadores[vez].vida++;
-        else if(tipoImgAtual == 1 && jogadores[vez].agua < 10)
+            jogadores[vez].saldo.sVida--;
+        }
+        else if(tipoImgAtual == 1 && jogadores[vez].agua < 10) {
             jogadores[vez].agua++;
-        else if(tipoImgAtual == 2 && jogadores[vez].defesa < 10)
+            jogadores[vez].saldo.sAgua--;
+        }
+        else if(tipoImgAtual == 2 && jogadores[vez].defesa < 10) {
             jogadores[vez].defesa++;
-        else if(tipoImgAtual == 3 && jogadores[vez].dinheiro < 10)
+            jogadores[vez].saldo.sDefesa--;
+        }
+        else if(tipoImgAtual == 3 && jogadores[vez].dinheiro < 10) {
             jogadores[vez].dinheiro++;
+            jogadores[vez].saldo.sDinheiro--;
+        }
 
         atualizaHTMLJogador(jogadores[vez]);
 
@@ -558,11 +734,6 @@ for (let i = 0; i < trocaAtNode.length; i++) {
     trocaAtNode[i].addEventListener("mousedown", geraAt);
 }
 
-for (let i = 0; i < atNode.length; i++) {
-    atNode[i].addEventListener("mousedown", geraAt);
-    atNode[i].style.cursor = "grab";
-}
-
 for (let i = 0; i < barraNode.length; i++) {
     barraNode[i].addEventListener("mouseover", checaBarra);
     barraNode[i].addEventListener("mouseout", () => {
@@ -575,4 +746,16 @@ for (let i = 0; i < trocaAtNode.length; i++) {
     trocaAtNode[i].style.cursor = "grab";
 }
 
-console.log(barraNode[0]);
+for (let i = 0; i < jogadores.length; i++) {        
+    efeitoCartaEvt(cartaEvtAtual, jogadores[i]);
+}
+
+for (let i = 0; i < trocaAtNode.length; i++) {
+    trocaAtNode[i].addEventListener("mousedown", geraAt);
+}
+
+for (let i = 0; i < atNode.length; i++) {
+    atNode = document.querySelectorAll(".fixo");
+    atNode[i].addEventListener("mousedown", geraAt);
+    atNode[i].style.cursor = "grab";
+}
